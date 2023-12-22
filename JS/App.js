6b5a7ddb248dbbe703da6be9d54f81aadd082f90
AngwenyiOgata <147700772+AngwenyiOgata@users.noreply.github.com>
2023-12-22 14:24:24 +0300

    Add files via upload

--- JS/App.js
+++ JS/App.js
@@ -1,1 +1,15 @@
-JS
+(function(){
+  'use strict';
+  const navLinks = document.querySelector('.app__mobile ul');
+  const hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', (evt) =>{
+    evt.preventDefault()
+    if (navLinks.classList.contains('hidden')) {
+      navLinks.className = 'app__header-ul';
+      hamburger.src = './images/icon-close.svg';
+    } else {
+      navLinks.className = 'hidden';
+      hamburger.src = './images/icon-hamburger.svg';
+    }
+  })
+}());
